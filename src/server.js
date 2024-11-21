5629bdadc664b280970fc792e3c8ed8a146b8854
ilhanmanzis <ilhanmanzis1207@gmail.com>
2024-11-21 22:22:25 +0700

    bug fixing

--- src/server.js
+++ src/server.js
@@ -15,34 +15,10 @@ const init = async()=>{
         }
     });
 
-    // Register plugins
-  const swaggerOptions = {
-    info: {
-      title: openApiSpec.info.title,
-      version: openApiSpec.info.version,
-      description: openApiSpec.info.description
-    },
-
-    documentationPath: '/documentation',
-    grouping: 'tags'
-  };
-
-    await server.register([
-    Inert,
-    Vision,
-    {
-      plugin: HapiSwagger,
-      options: swaggerOptions
-    }
-  ]); 
-
-
     server.route(routes);
     await server.start();
     console.log("============================================================");  
     console.log(`server run is ${server.info.uri}`);
-    console.log("============================================================");
-    console.log(`Documentations Api run is ${server.info.uri}/documentation`);
     console.log("============================================================");
 }
 
