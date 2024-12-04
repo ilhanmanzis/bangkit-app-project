7e90ede5e49ac10acc5a0d2609e213a221923a95
ilhanmanzis <ilhanmanzis1207@gmail.com>
2024-12-04 20:27:40 +0700

    clean code dan merapikan log eror

--- src/services/renameMakanan.js
+++ src/services/renameMakanan.js
@@ -1,5 +1,5 @@
 const renameMakanan = async(makanan)=>{
-    const class_labels = ["ayam bakar", "ayam_goreng", "ayam_pop", "bakso", "daging_rendang","dendeng_batakok", "gado_gado", "grontol", "gulai_ikan", "gulai_tambsu","gulai_tunjang", "lanting", "lumpia", "putu_ayu", "serabi_solo", "telur_balado", "telor_dadar", "wajik"]
+    const class_labels = ['ayam bakar', 'ayam_goreng', 'ayam_pop', 'bakso', 'daging_rendang','dendeng_batakok', 'gado_gado', 'grontol', 'gulai_ikan', 'gulai_tambsu','gulai_tunjang', 'lanting', 'lumpia', 'putu_ayu', 'serabi_solo', 'telur_balado', 'telor_dadar', 'wajik']
 
     const nama = ["Ayam Bakar", "Ayam Goreng", "Ayam Pop", "Bakso", "Daging Rendang","Dendeng Batakok", "Gado Gado", "Grontol", "Gulai Ikan", "Gulai Tambsu","Gulai Tunjang", "Lanting", "Lumpia", "Putu Ayu", "Serabi Solo", "Telur Balado", "Telor Dadar", "Wajik"]
         
